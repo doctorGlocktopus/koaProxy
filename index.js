2b3f8b37e107c8acae15254215d7386964d5122e
Fabian Laske <104904431+doctorGlocktopus@users.noreply.github.com>
2022-11-24 20:36:51 +0100

    d

--- index.js
+++ index.js
@@ -3,17 +3,45 @@ const Koa = require("koa");
 const app = new Koa();
 const port = process.env.PORT || 8000;
 const cors = require("@koa/cors");
-const proxy = require('koa-proxy');
+const proxy = require('koa-proxies') // maybe state of the art
+
+
+
+// https://thewebdev.info/2020/07/21/sending-response-with-koa/
+// const Koa = require('koa');
+// const app = new Koa();
+//
+// app.use(async (ctx, next) => {
+//     ctx.set('Access-Control-Allow-Origin', '*');
+//     ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//     ctx.set('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+//     ctx.body = 'hello';
+// });
+//
+// app.listen(3000);
+
+
+//const fs = require('fs');
+// benutze fs für die bearbeitung der Datei
+
+
+
 
 // https://www.auto-motor-und-sport.de/thenewsbar/static/pw.js
 // https://pw.thenewsbar.net/static/pw.js
-// https://github.com/edorivai/koa-proxy
 
 
 app.use(cors())
-app.use(proxy({
-    host: 'http://google.de'
-}));
+
+// path url beeinflusst destination
+
+// get string lesen und danach das Target richten
+
+app.use(proxy('/thenewsbar/static/pw.js', {
+    target: 'https://www.auto-motor-und-sport.de',
+    changeOrigin: true
+}))
+
 app.listen(port, function () {
     console.log(`listening on port ${port}`);
 })

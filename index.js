51118da066132fad29bfb548a52018804f50b891
Fabian Laske <104904431+doctorGlocktopus@users.noreply.github.com>
2022-11-28 16:01:54 +0100

    guterCommit

--- index.js
+++ index.js
@@ -1,46 +1,49 @@
 require('dotenv').config();
+const cors = require("@koa/cors");
+const proxy = require('koa-proxies');
 const Koa = require("koa");
 const app = new Koa();
 const port = process.env.PORT || 8000;
-const cors = require("@koa/cors");
-const proxy = require('koa-proxies') // maybe state of the art
 
+app.use(cors())
 
-
-// https://thewebdev.info/2020/07/21/sending-response-with-koa/
-// const Koa = require('koa');
-// const app = new Koa();
-//
-// app.use(async (ctx, next) => {
-//     ctx.set('Access-Control-Allow-Origin', '*');
-//     ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//     ctx.set('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
-//     ctx.body = 'hello';
-// });
-//
-// app.listen(3000);
-
-
-//const fs = require('fs');
-// benutze fs für die bearbeitung der Datei
+let value = "https://www.auto-motor-und-sport.de"
 
 
 
 
+app.use(async (ctx, next) => {
+    // let plane = await fetch(value + '/thenewsbar/static/pw.js')
+    // await plane.replace(value + '/thenewsbar',"https://pw.thenewsbar.net")
+    // await plane.replace(value + '/metricsthenewsbar/metrics/',"https://pw.thenewsbar.net/static/pw.js")
+    ctx.set('Access-Control-Allow-Origin', '*');
+    ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    ctx.set('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    ctx.body = "sell";
+
+    ctx.body = await fetch(value + '/thenewsbar/static/pw.js')
+        .then((res)=>{
+            return res.text();
+        }).then((text)=>{
+            let one = text.replace(value + '/thenewsbar',"https://pw.thenewsbar.net")
+            let two = one.replace(value + '/metricsthenewsbar/metrics/',"https://pw.thenewsbar.net/static/pw.js")
+            return two
+        })
+});
+
+
+// app.use(proxy('/thenewsbar/static/pw.js', {
+//     // jetzt reinlesen in die Datei und eine Ausgabe definieren
+//     target: 'https://www.auto-motor-und-sport.de',
+//     changeOrigin: true
+// }))
 // https://www.auto-motor-und-sport.de/thenewsbar/static/pw.js
 // https://pw.thenewsbar.net/static/pw.js
 
 
-app.use(cors())
 
-// path url beeinflusst destination
 
-// get string lesen und danach das Target richten
 
-app.use(proxy('/thenewsbar/static/pw.js', {
-    target: 'https://www.auto-motor-und-sport.de',
-    changeOrigin: true
-}))
 
 app.listen(port, function () {
     console.log(`listening on port ${port}`);

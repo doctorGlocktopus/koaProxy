8ad7978096c9ba43ca4cef1238fa1b7b2f75436a
Fabian Laske <104904431+doctorGlocktopus@users.noreply.github.com>
2022-12-05 22:40:45 +0100

    littleChange

--- index.js
+++ index.js
@@ -7,32 +7,7 @@ const port = process.env.PORT;
 const PATH_DOMAIN_MAP = require('./paths');
 console.log(PATH_DOMAIN_MAP)
 
-// app.use(cors())
-
-// erster Schritt
-
-// http://localhost:8000/googleAdd/analytics.js
-// http://localhost:8000/ams/static/pw.js
-
-const REPLACEMENT_DOMAIN = "http://localhost:8000";
-
-
-// PATH_DOMAIN_MAP
-// const PATH_DOMAIN_MAP = {
-//     "googletagmanager": {
-//         url: "https://www.googletagmanager.com",
-//         proxyPath: "/gtm.js?id=GTM-P9H4MZM",
-//     },
-//     'google-analytics': {
-//         url: "https://www.google-analytics.com",
-//         proxyPath: "/analytics.js",
-//     },
-//     'thenewsbar': {
-//         url: "https://pw.thenewsbar.net",
-//         proxyPath: '/static/pw.js',
-//     },
-// };
-
+app.use(cors())
 
 app.use(async (ctx, next) => {
 
